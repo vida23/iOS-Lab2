d2caabb4591ce0ced86a0018c7abc1f125b71555
vida23 <vidageetirashidi@gmail.com>
2019-12-06 12:15:48 +0100

    new push

---
+++ iOSLab2/TableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TableViewController.swift
+//  iOSLab2
+//
+//  Created by Vida Rashidi on 2019-12-05.
+//  Copyright © 2019 Vida Rashidi. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+  
+    
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ iOSLab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLab2
+//
+//  Created by Vida Rashidi on 2019-12-05.
+//  Copyright © 2019 Vida Rashidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
